974b04aed518c6e0ba04ab8b379f18a737add245
PetrVakulenko <vakulenkopetya@gmail.com>
2018-10-07 20:15:04 +0300

    [JSSchool] React redux todolist

---
+++ reduxTodolist/src/ToDoList/AddTodo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AddTodo = ({ onAddTodo }) => {
+  let todoInput = null;
+  return (
+    <div className="form-group form-inline">
+      <input
+        className="form-control bs-popover-left"
+        type="text"
+        ref={(node) => { todoInput = node;}}
+      />
+      <button type="button" className="btn btn-primary" onClick={() => {
+        onAddTodo(todoInput.value);
+        todoInput.value = '';
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default AddTodo;
